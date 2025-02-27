6a8a7acf18d33d27ddb1c0f7b0751bc9a2ef3ccd
masato619 <masa619biz@outlook.jp>
2025-02-28 05:38:01 +0900

    second commit

--- src/database/migrations/2025_02_12_224402_create_lyrics_table.php
+++ src/database/migrations/2025_02_12_224402_create_lyrics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKashiTable extends Migration
+class CreateLyricsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateKashiTable extends Migration
      */
     public function up()
     {
-        Schema::create('kashi', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('lyrics', function (Blueprint $table) {
+            $table->string('title');
+            $table->string('artist');
+            $table->text('lyrics');
         });
     }
 
@@ -26,6 +27,6 @@ class CreateKashiTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kashi');
+        Schema::dropIfExists('lyrics');
     }
 }
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>歌詞検索</h2>
+    </div>
+    <div class="form__group">
+        <div class="" </div>
+        </div>
+
+        @endsection
